e8dbce8668b1879c44a4bfc30de5509b0af54033
Bruno Gianini <blgianini@gmail.com>
2023-09-13 18:04:33 -0300

    docker

--- src/main/java/br/com/avanade/DesafioRpg/DesafioRpgApplication.java
+++ src/main/java/br/com/avanade/DesafioRpg/DesafioRpgApplication.java
@@ -3,11 +3,15 @@ package br.com.avanade.DesafioRpg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
 public class DesafioRpgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioRpgApplication.class, args);
 	}
+
 
 }
